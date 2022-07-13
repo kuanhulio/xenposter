816328b96792a102533996029c952bf7ad7f2214
Sami Altamimi <sami.n.altamimi@gmail.com>
2022-07-13 17:54:43 -0500

    base: bump version and rebuild builder file

---
+++ builder.sh
@@ -0,0 +1,2 @@
+pyinstaller --distpath "/mnt/c/Users/mrsam/Documents/Projects/xenposter/linux/dist" --workpath "/mnt/c/Users/mrsam/Documents/Projects/xenposter/linux/build" --noconfirm "/mnt/c/Users/mrsam/Documents/Projects/xenposter/xenposter/xenposter.py" && \
+powershell.exe pyinstaller --distpath "C:\Users\mrsam\Documents\Projects\xenposter\windows\dist" --workpath "C:\Users\mrsam\Documents\Projects\xenposter\windows\build" --noconfirm "C:\Users\mrsam\Documents\Projects\xenposter\xenposter\xenposter.py"
